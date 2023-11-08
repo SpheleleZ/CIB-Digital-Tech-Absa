5081dbde6c4f55922c8a6c0a32a2486819661d17
Sphelele <Sphelelez101@gmail.com>
2023-11-08 10:05:38 +0200

    add test verifyRetrieverBreedIsWithinTheList

--- src/test/java/org/cib/digital/tech/absa/api/DogApiTest.java
+++ src/test/java/org/cib/digital/tech/absa/api/DogApiTest.java
@@ -1,6 +1,8 @@
 package org.cib.digital.tech.absa.api;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.baseURI;
@@ -22,5 +24,29 @@ public class DogApiTest {
                 .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void verifyRetrieverBreedIsWithinTheList() {
+
+        baseURI = "https://dog.ceo/api/";
+
+        Response response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .pathParam("myPath", "breeds")
+                        .pathParam("myPath1", "list")
+                        .pathParam("myPath2", "all")
+                        .when()
+                        .get("{myPath}/{myPath1}/{myPath2}");
+
+        JSONObject jsonObject = new JSONObject(response.asString());
+
+        String message = jsonObject.getJSONObject("message").toString();
+
+        String retrieve = "retrieve";
+        if (message.contains(retrieve)) {
+            System.out.println("Verify - " + retrieve + " is within List");
+        }
     }
 }
